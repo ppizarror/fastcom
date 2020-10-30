0e8643c85a9245001173e309aae113aebea45355
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-10-29 22:12:35 -0300

    v100

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # fastcom
-API that lets get the results from fast.com test using Selenium
+
+API that lets get the results from fast.com test using Selenium.
+
+## Install
+
+pyfastcom can be installed via pip. Simply run:
+
+```bash
+pip install pyfastcom
+```
+
+## Usage
+
+To create a searcher object, use the *PyFastCom* class:
+
+```python
+from pyfastcom import PyFastCom
+
+fast = PyFastCom()
+```
+
+To configure the path of the webdriver (Chrome):
+
+```python
+fast.set_driver_path('path/to/webdriver/executable/')
+```
+
+Then, execute the `.run(timeout=120)` method. This function takes the timeout as an input. That is the maximum number of seconds before throwing an Exception. By default, the timeout is 240 s (2 minutes). The function returns the following dictionary:
+
+```python
+results = fast.run()
+print(results) # {'client_ip': 'XXX', 'client_isp': 'XXX', 'client_location': 'XXX', 'download': (100, 'Mbps'), 'latency_download': (1, 'ms'), 'latency_upload': (1, 'ms'), 'server_info': 'XXX', 'upload': (100, 'Mbps')}
+```
+
+Also, the results can be obtained through the following methods:
+
+- PyFastCom.*get_client_ip()*: Returns the client IP (str).
+- PyFastCom.*get_client_isp()*: Returns the client ISP (str).
+- PyFastCom.*get_client_location()*: Returns the client location (str).
+- PyFastCom.*get_download_speed()*: Returns the download speed (int, str).
+- PyFastCom.*get_download_latency()*: Returns the download latency (int, str).
+- PyFastCom.*get_upload_speed()*: Returns the upload speed (int, str).
+- PyFastCom.*get_upload_latency()*: Returns the upload latency (int, str).
+
+Check out the provided example to see the complete usage scenario.
---
+++ example.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+"""
+pyfastcom
+https://github.com/ppizarror/pyfastcom
+PYFASTCOM EXAMPLE
+
+License:
+-------------------------------------------------------------------------------
+The MIT License (MIT)
+Copyright 2017-2020 Pablo Pizarro R. @ppizarror
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-------------------------------------------------------------------------------
+"""
+
+from pyfastcom import PyFastCom
+
+fast = PyFastCom()
+# Set the driver folder
+fast.set_driver_path('../.../../../../Software/WEBDRIVERS/chromedriver_win32/v86/')
+print(fast.run())
+
+print(fast.get_client_ip())
+print(fast.get_client_isp())
+print(fast.get_client_location())
+print(fast.get_download_speed())
+print(fast.get_download_latency())
+print(fast.get_upload_speed())
+print(fast.get_upload_latency())
+print(fast.get_server_info())
